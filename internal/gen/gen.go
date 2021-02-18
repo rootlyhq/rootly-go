c599b58e7e647de9a1fd08147d70cb21ee3784fd
Matthew Gleich <git@mattglei.ch>
2021-02-18 16:52:43 -0500

    ✨ Get swagger config from website
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- internal/gen/gen.go
+++ internal/gen/gen.go
@@ -5,20 +5,64 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-const binaryName = "oapi-codegen"
+const (
+	binaryName  = "oapi-codegen"
+	swaggerFile = "swagger.json"
+	swaggerURL  = "https://rootly.io/swagger/v1/swagger.json"
+)
 
 func main() {
+	getSwaggerConfig()
+	genCode()
+	cleanUp()
+}
+
+// Get the swagger config from rootly.io/api
+func getSwaggerConfig() {
+	// Making request
+	log.Println("Getting latest swagger config from rootly.io/api")
+	resp, err := http.Get(swaggerURL)
+	if err != nil {
+		log.Fatal("Failed to get latest swagger config from ", swaggerURL, "\n\n", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("Failed to get latest swagger config from ", swaggerURL, "with exit code of ", resp.Status)
+	}
+	log.Println("Got latest swagger data from rootly.io/api")
+
+	// Decoding the body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Failed to parse data from ", swaggerURL, "\n\n", err)
+	}
+
+	// Writing the body to ./swagger.json
+	err = ioutil.WriteFile(swaggerFile, body, 0666)
+	if err != nil {
+		log.Fatal("Failed to write to ", swaggerFile, "\n\n", err)
+	}
+	log.Println("Wrote swagger config to swagger.json")
+}
+
+// Gen the schema.gen.go file
+func genCode() {
+	log.Println("Generating schema.gen.go")
+
+	// Getting the codegen tool path
 	codegenPath, err := exec.LookPath(binaryName)
 	if err != nil {
 		log.Fatal("Is ", binaryName, " installed? ", "\n\n", err)
 	}
 
-	cmd := exec.Command(codegenPath, "swagger.json")
+	// Running the command and getting the stdout
+	cmd := exec.Command(codegenPath, swaggerFile)
 	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	if err != nil {
@@ -26,9 +70,19 @@ func main() {
 	}
 	output = []byte(strings.Replace(string(output), "package Swagger", "package rootly", 1))
 
+	// Writing the stdout to the schema.gen.go file
 	err = ioutil.WriteFile("../../schema.gen.go", []byte(output), 0666)
 	if err != nil {
 		log.Fatal("Failed to write data to scheme file", "\n\n", err)
 	}
+	log.Println("Generated schema.gen.go")
+}
 
+// Remove the ./swagger.json file
+func cleanUp() {
+	err := os.Remove(swaggerFile)
+	if err != nil {
+		log.Fatal("Failed to delete ", swaggerFile, "\n\n", err)
+	}
+	log.Println("Removed swagger.json")
 }
