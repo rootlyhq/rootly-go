1a68eab2cc2575744778cd2b7a0d66a3ac71bcc0
Jamie Tanna <jamie.tanna@rootly.com>
2026-05-05 11:04:39 +0100

    refactor: move embedded OpenAPI spec into `spec.gen.go`
    To reduce diff noise, we can move it into its own file.

--- generate.go
+++ generate.go
@@ -1,2 +1,3 @@
 //go:generate go run -modfile=./tools/go.mod github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml .vendored/rootly-api/swagger.json
+//go:generate go run -modfile=./tools/go.mod github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config-spec.yaml .vendored/rootly-api/swagger.json
 package rootly
